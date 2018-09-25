b613aa88875f491c2478f985d6b4893fedae947c
Emmanouil Stamatakis <TheMagicSport@gmail.com>
2018-09-25 22:32:06 +0300

    old browser responsiveness

--- StartingPage/js/responsive-nav.js
+++ StartingPage/js/responsive-nav.js
@@ -8,17 +8,11 @@
 
 /* global Event */
 (function (document, window, index) {
-  // Index is used to keep multiple navs on the same page namespaced
-
   "use strict";
 
   var responsiveNav = function (el, options) {
 
     var computed = !!window.getComputedStyle;
-    
-    /**
-     * getComputedStyle polyfill for old browsers
-     */
     if (!computed) {
       window.getComputedStyle = function(el) {
         this.el = el;
