0a68c3e57746d3cd6022760075a0cd28cf225ce2
DOMAIN\eas <e.stamatakis@dataverse.gr>
2018-10-19 14:51:38 +0300

    WTF is going on

--- StartingPage/js/Charts.js
+++ StartingPage/js/Charts.js
@@ -6,17 +6,10 @@ function showChart(chartDataPoints, chartTitle) {
       title:{
         text: chartTitle   
       },
-<<<<<<< HEAD
       data: [             
         {
          type: document.getElementById("typeOfChartSelection").value,
          dataPoints : chartDataPoints
-=======
-      data: [        
-        {
-         type: document.getElementById("typeOfChartSelection").value,
-         dataPoints : contentFilters()
->>>>>>> dfa1e08e3936e07969cc67b99492ca4a2681070a
          }
        ],
      });
@@ -75,13 +68,7 @@ function contentFilters(){
     return {label: i.description, y: parseFloat(i[" amount "])};
   });
 
-<<<<<<< HEAD
   showChart(filtersChartValues, "amount of money has been spend");
-=======
-
-function Refresh(){
-  showChart();
->>>>>>> dfa1e08e3936e07969cc67b99492ca4a2681070a
 }
 //#endregion
 
